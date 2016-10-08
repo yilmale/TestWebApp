d7be36a3c93e3ebe971998034ea207c886afdf6c
yilmale <yilmaz@auburn.edu>
2016-10-08 12:37:38 -0500

    update imports

--- src/main/scala/au/example/app/MyScalatraServlet.scala
+++ src/main/scala/au/example/app/MyScalatraServlet.scala
@@ -6,14 +6,18 @@ import org.scalatra._
 import edu.uci.ics.jung.graph.util.Graphs
 import edu.uci.ics.jung.graph.{Graph, ObservableGraph, UndirectedSparseMultigraph}
 
+
+
 class MyScalatraServlet extends TestWebAppStack  {
   get("/") {
-
+    val x = txt.test.render("testtemplate").toString()
+    println(x)
     html.index(new java.util.Date)
+
   }
 
   get("/entities") {
-    ParseExpr1.parseExpression("1+2*3")
+    ParseExpr1.parseExpression("1+4*3")
     Ok("Got request [" + "]")
   }
 
