1c49f92a965548f69cb278eb7951fcdbeaad190b
yilmale <yilmaz@auburn.edu>
2016-10-10 06:52:00 -0500

    Merge branch 'master' of https://github.com/yilmale/TestWebApp
    # Conflicts:
    #	src/main/scala/au/example/app/MyScalatraServlet.scala

--- src/main/scala/au/example/app/MyScalatraServlet.scala
+++ src/main/scala/au/example/app/MyScalatraServlet.scala
@@ -6,6 +6,15 @@ import org.scalatra._
 import edu.uci.ics.jung.graph.util.Graphs
 import edu.uci.ics.jung.graph.{Graph, ObservableGraph, UndirectedSparseMultigraph}
 
+import reflect.runtime.currentMirror
+import tools.reflect.ToolBox
+
+
+case class Person(name:String)
+
+
+import scala.reflect.runtime.universe._
+
 class MyScalatraServlet extends TestWebAppStack  {
   get("/") {
 
@@ -14,7 +23,44 @@ class MyScalatraServlet extends TestWebAppStack  {
 
   post("/parsemodel") {
     val modelSrc = params("model")
-    NetExpr.parseExpression(modelSrc)
+    //NetExpr.parseExpression(modelSrc)
+    Eval[Unit](modelSrc)
+
+    val bFactory = BehaviorFactory("/Users/yilmaz/external.scala")
+    val b = bFactory.make()
+    val p = b.getBehavior()
+
+
+
+    println(p)
+
+    val ru = scala.reflect.runtime.universe
+    val m = ru.runtimeMirror(getClass.getClassLoader)
+    val classPerson = ru.typeOf[Person].typeSymbol.asClass
+    val cm = m.reflectClass(classPerson)
+    val ctor = ru.typeOf[Person].decl(ru.termNames.CONSTRUCTOR).asMethod
+    val ctorm = cm.reflectConstructor(ctor)
+    val t = ctorm("Levent")
+
+    println(t.asInstanceOf[Person].name)
+
+    val mysrc = txt.test("testtemplate").toString()
+
+    println(mysrc)
+
+    val execsrc = txt.execmodel("au.example.app._","A1",modelSrc).toString()
+
+    println(execsrc)
+
+
+    val pFactory = PointFactory(execsrc)
+    val pb = pFactory.make()
+    val myp = pb.getBehavior()
+
+    println(myp)
+
+    myp.y()
+
     Ok("Parsed data")
   }
 
@@ -51,10 +97,13 @@ class MyScalatraServlet extends TestWebAppStack  {
        }
      }
       """
-    NetExpr.parseExpression(inpNet)
+    //NetExpr.parseExpression(inpNet)
 
     //val x = html.index.render(new java.util.Date).toString()
     //println(x)
+
+
+
     Ok("Got request [" + "]")
   }
 
